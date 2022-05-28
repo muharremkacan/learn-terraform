90497267a782455c60297fcf2e3025d707d846ae
muharrem-kacan <muharrem@gameon.app>
2022-05-28 23:21:28 +0300

    aaa

--- main.tf
+++ main.tf
@@ -2,17 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami_ids" "ubuntu" {
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
-  }
-
-  owners = ["self"] # Canonical
-}
 
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami_ids.ubuntu.id
+  ami           = "ami-09d56f8956ab235b3"
   instance_type = var.instance_type
 
   tags = {
