071dba8b7db56aeb67f51f25346475c349db2ff6
muharrem-kacan <muharrem@gameon.app>
2022-05-29 00:11:17 +0300

    dfd

--- versions.tf
+++ versions.tf
@@ -2,9 +2,10 @@ terraform {
   required_version = ">= 0.12"
 
   backend "s3" {
-    bucket = "my-tf-test-bucket-111"
+    bucket = "timothybucket2123"
     key = "xxx.tfstate"
     region = "us-east-1"
+ #   dynamodb_table = "timothytest"
   }
   
 }
